38e65ac05475c5aa22ca9636ba3fcc78ba6e0320
ChamithLakshan <88126250+ChamithLakshan@users.noreply.github.com>
2023-01-07 18:15:50 +0530

    signup page connected with database

--- login/views.py
+++ login/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Intern
 
 # Create your views here.
 def login(request):
@@ -7,3 +8,22 @@ def login(request):
 
 def signup(request):
     return render(request, 'signup.html')
+
+
+def signup_intern(request):
+
+    if request.method == 'POST':
+        intern = Intern()
+        intern.firstname = request.POST['first_name']
+        intern.lastname = request.POST['last_name']
+        intern.username = request.POST['username']
+        intern.email = request.POST['email']
+        intern.password = request.POST['password1']
+        pw2 = request.POST['password2']
+        intern.save()
+
+        return render(request, 'login.html')
+
+        
+    else:
+        return render(request, 'signup.html')
